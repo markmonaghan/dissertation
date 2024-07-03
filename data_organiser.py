2b515d0dbfc869be97c90933a7c0dc62c5671a63
mrm2002 <mark.monaghan@hw.ac.uk>
2024-07-03 15:05:24 +0100

    removing unused scripts

--- data_organiser.py
+++ data_organiser.py
@@ -32,7 +32,7 @@ def create_directories(directories):
     print("All directories have been created.")
 
 # Call the function to create the directories
-# create_directories(directories)
+create_directories(directories)
 
 def count_image_labels(df):
     """
@@ -58,22 +58,22 @@ def count_image_labels(df):
     print(f"Number of images with the label 'Non-Fibrosis': {non_fibrosis_count}")
 
 # Call the function to count the number of images based on their labels
-# count_image_labels(df)
+count_image_labels(df)
 
 # List of directories on the local filesystem where the NIH CXR images are stored
-# images_directory = [
-    # r'C:\Users\Mark\Documents\rp\nih_dataset\images_001\images' 
-    # r'C:\Users\Mark\Documents\rp\nih_dataset\images_002\images', 
-    # r'C:\Users\Mark\Documents\rp\nih_dataset\images_003\images',
-    # r'C:\Users\Mark\Documents\rp\nih_dataset\images_004\images',
-    # r'C:\Users\Mark\Documents\rp\nih_dataset\images_005\images',
-    # r'C:\Users\Mark\Documents\rp\nih_dataset\images_006\images',
-    # r'C:\Users\Mark\Documents\rp\nih_dataset\images_007\images',
-    # r'C:\Users\Mark\Documents\rp\nih_dataset\images_008\images',
-    # r'C:\Users\Mark\Documents\rp\nih_dataset\images_009\images',
-    # r'C:\Users\Mark\Documents\rp\nih_dataset\images_010\images',
-    # r'C:\Users\Mark\Documents\rp\nih_dataset\images_011\images',
-    # r'C:\Users\Mark\Documents\rp\nih_dataset\images_012\images' ]
+images_directory = [
+    r'C:\Users\Mark\Documents\rp\nih_dataset\images_001\images' 
+    r'C:\Users\Mark\Documents\rp\nih_dataset\images_002\images', 
+    r'C:\Users\Mark\Documents\rp\nih_dataset\images_003\images',
+    r'C:\Users\Mark\Documents\rp\nih_dataset\images_004\images',
+    r'C:\Users\Mark\Documents\rp\nih_dataset\images_005\images',
+    r'C:\Users\Mark\Documents\rp\nih_dataset\images_006\images',
+    r'C:\Users\Mark\Documents\rp\nih_dataset\images_007\images',
+    r'C:\Users\Mark\Documents\rp\nih_dataset\images_008\images',
+    r'C:\Users\Mark\Documents\rp\nih_dataset\images_009\images',
+    r'C:\Users\Mark\Documents\rp\nih_dataset\images_010\images',
+    r'C:\Users\Mark\Documents\rp\nih_dataset\images_011\images',
+    r'C:\Users\Mark\Documents\rp\nih_dataset\images_012\images' ]
 
 def process_images_in_directories(image_directory):
     """
@@ -86,20 +86,20 @@ def process_images_in_directories(image_directory):
     for filename in os.listdir(image_directory):
         image_path = os.path.join(image_directory, filename)   
                 
-    #     for index, row in df.iterrows():       
-    #         if filename == row['Image Index']:
-    #             if 'Fibrosis' in row['Finding Labels']:
-    #                 # print(f"Moving image: {row['Image Index']} to fibrosis folder")
-    #                 shutil.move(image_path, os.path.join(image_directory, 'fibrosis', filename))
-    #                 # print(f"Fibrosis image found: {row['Finding Labels']}, {row['Image Index']}, Patient ID: {row['Patient ID']}")
-    #             elif 'No Finding' in row['Finding Labels']:
-    #                 # print(f"Moving image: {row['Image Index']} to no_finding folder")
-    #                 shutil.move(image_path, os.path.join(image_directory, 'no_finding', filename))
-    #             else:
-    #                 # print(f"Moving image: {row['Image Index']} to non_fibrosis folder")
-    #                 shutil.move(image_path, os.path.join(image_directory, 'non_fibrosis', filename))
+        for index, row in df.iterrows():       
+            if filename == row['Image Index']:
+                if 'Fibrosis' in row['Finding Labels']:
+                    # print(f"Moving image: {row['Image Index']} to fibrosis folder")
+                    shutil.move(image_path, os.path.join(image_directory, 'fibrosis', filename))
+                    # print(f"Fibrosis image found: {row['Finding Labels']}, {row['Image Index']}, Patient ID: {row['Patient ID']}")
+                elif 'No Finding' in row['Finding Labels']:
+                    # print(f"Moving image: {row['Image Index']} to no_finding folder")
+                    shutil.move(image_path, os.path.join(image_directory, 'no_finding', filename))
+                else:
+                    # print(f"Moving image: {row['Image Index']} to non_fibrosis folder")
+                    shutil.move(image_path, os.path.join(image_directory, 'non_fibrosis', filename))
 
-    # print("All images have been processed.")
+    print("All images have been processed.")
 
 # Call the function to process the images
 process_images_in_directories(image_directory)
